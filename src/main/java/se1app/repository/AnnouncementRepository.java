70bc4a089fc3064df5a1e5c714e5b141c3870f83
Neak <legendmax@live.de>
2016-11-06 16:28:38 +0100

    tried to implement Announcementrepository

--- src/main/java/se1app/repository/AnnouncementRepository.java
+++ src/main/java/se1app/repository/AnnouncementRepository.java
@@ -1,10 +1,23 @@
 package se1app.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import se1app.entitie.Announcement;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by Neak on 06.11.2016.
  */
 @Repository
-public class AnnouncementRepository {
+public interface AnnouncementRepository extends JpaRepository<Announcement, Integer> {
+    // Spring leitet die Query aus der Signatur ab ("name" ist ein Attribut von Customer)
+    Optional<Announcement> findByUser(Integer id);
+
+    // Hier definieren wir eine eigene SQL-Query ("native")
+    @Query(value = "SELECT * FROM ANNOUNCEMENT WHERE USER.ID= 2", nativeQuery = true)
+    List<Announcement> findAnnouncementFromUser(@Param("id") Integer id);
 }
