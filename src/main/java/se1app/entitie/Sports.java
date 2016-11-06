d918b81bffbc2dafb10d70c59cf8de36b96cd495
Neak <legendmax@live.de>
2016-11-06 15:42:42 +0100

    added repositories and @entity

--- src/main/java/se1app/entitie/Sports.java
+++ src/main/java/se1app/entitie/Sports.java
@@ -1,7 +1,10 @@
 package se1app.entitie;
 
+import javax.persistence.Entity;
+
 /**
  * Created by MattX7 on 06.11.2016.
  */
+@Entity
 public enum Sports {
 }
---
+++ src/main/java/se1app/entitie/SportsRepository.java
@@ -0,0 +1,10 @@
+package se1app.entitie;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Neak on 06.11.2016.
+ */
+@Repository
+public class SportsRepository {
+}
